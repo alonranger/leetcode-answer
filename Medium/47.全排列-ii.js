bf098220ad9c3fd3ce02583b82a1ea955bb2f145
lee <248197941@qq.com>
2022-04-12 16:15:20 +0800

    重复元素全排列优化

--- Medium/47.全排列-ii.js
+++ Medium/47.全排列-ii.js
@@ -12,24 +12,50 @@
 var permuteUnique = function (nums) {
   if (!nums.length) return nums;
   let ans = [];
-  let set = new Set();
-  function backTrack(list, source) {
+  // let set = new Set();
+  // function backTrack(list, source) {
+  //   if (list.length == nums.length) {
+  //     let str = list.slice().join("");
+  //     if (!set.has(str)) {
+  //       ans.push(list.slice());
+  //       set.add(str);
+  //     }
+  //     return;
+  //   }
+  //   for (let i = 0; i < source.length; i++) {
+  //     list.push(source[i]);
+  //     let newSource = source.slice();
+  //     newSource.splice(i,1)
+  //     backTrack(list, newSource);
+  //     list.pop();
+  //   }
+  // }
+
+  // 优化后
+  let visited = new Array(nums.length).fill(false);
+  nums.sort((a, b) => a - b);
+  function backTrack(list, visited) {
     if (list.length == nums.length) {
-      let str = list.slice().join("");
-      if (!set.has(str)) {
-        ans.push(list.slice());
-        set.add(str);
-      }
+      ans.push(list.slice());
       return;
     }
-    for (let i = 0; i < source.length; i++) {
-      list.push(source[i]);
-      let newSource = source.slice();
-      newSource.splice(i,1)
-      backTrack(list, newSource);
+    for (let i = 0; i < nums.length; i++) {
+      // nums[i] == nums[i - 1] && visited[i - 1] == false 跳过当前层级循环中相等的元素
+      // 如果visited[i-1]==true说明当前元素是在前一个元素的下一级
+      if (
+        (i > 0 && nums[i] == nums[i - 1] && visited[i - 1] == false) ||
+        visited[i]
+      )
+        continue;
+      list.push(nums[i]);
+      visited[i] = true;
+      backTrack(list, visited);
       list.pop();
+      visited[i] = false;
     }
   }
+  backTrack([], visited);
+
   backTrack([], nums);
   return ans;
 };
